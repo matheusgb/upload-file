cfce2231f15f2a46979728146a83ea6ff3482c36
matheusgb <matheusgbr98@gmail.com>
2022-09-08 01:53:31 -0300

    :bomb: redone with net/http

--- main.go
+++ main.go
@@ -1,15 +1,36 @@
 package main
 
 import (
-	"github.com/matheusgb/pocPostFile/handlers"
-	routing "github.com/qiangxue/fasthttp-routing"
-	"github.com/valyala/fasthttp"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
+func uploadFile(writer http.ResponseWriter, request *http.Request) {
+
+	request.ParseMultipartForm(10 << 20)     // memoria disponivel por requisição (10 mb)
+	file, _, err := request.FormFile("file") // key da requisição
+	if err != nil {
+		fmt.Println("Erro ao receber o arquivo")
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	tempFile, err := ioutil.TempFile("uploads", "file-*.png") // diretório e nome do arquivo
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer tempFile.Close()
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+	tempFile.Write(fileBytes)
+	fmt.Fprintf(writer, "Upload feito com sucesso\n")
+}
+
 func main() {
-	router := routing.New()
-
-	router.Get("/upload", handlers.GetFile)
-
-	fasthttp.ListenAndServe(":8080", router.HandleRequest)
+	http.HandleFunc("/upload", uploadFile)
+	http.ListenAndServe(":8080", nil)
 }
